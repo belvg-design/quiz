718518e43b34531793c764343a78c7e3e7f547d6
Glitch (quintessential-cat-music) <none>
2020-07-30 13:43:15 +0000

    🎰🍵 Checkpoint ./question.json:5361929/3889 ./script.js:5361929/1462

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ let start = document.querySelector("#start"),
     sectionQuestion = document.querySelector("#sectionQuestion"),
     sectionEnd = document.querySelector("#sectionEnd");
 
+let questions,
+    answers = [];
 
-start.addEventListener("click",(e)=>{
-    sectionStart.style.setProperty("display", "none");
-    sectionQuestion.style.setProperty("display", "flex");
-})
+fetch("questions.json")
+    .then(response => response.json())
+    .then(json =>{
+        questions = json;
+        start.addEventListener("click",(e)=>{
+            sectionStart.style.setProperty("display", "none");
+            sectionQuestion.style.setProperty("display", "flex");
+        })
+    });
+
+nextQuestion(number){
+    questionNumber.innerText = `Question ${number+1}/${questions.length}`;
+    question.innerText = questions[number].quest;
+    description.innerText = questions[number].descr;
+    input = answers[number]?answers[number]
+}
 
 forward.addEventListener("click",(e)=>{
     sectionQuestion.style.setProperty("display", "none");
